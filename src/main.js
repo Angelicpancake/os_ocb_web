250c2b6e667376c8c3b8bb9708431957dd141463
Angelicpancake <145518259+Angelicpancake@users.noreply.github.com>
2025-04-05 18:33:50 -0400

    .

---
+++ src/main.js
@@ -0,0 +1,42 @@
+/*          <div class="nav-buttons">
+                <img src="../assets/about.png" class="aboutB">
+                <img src="../assets/projects.png" class="projectsB">
+                <img src="../assets/team.png" class="teamB">
+            </div>*/
+
+//scroll functionality
+document.getElementById("projectsB").addEventListener("click", function() {
+    document.getElementById("page2").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("teamB").addEventListener("click", function() {
+    document.getElementById("page3").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("aboutB").addEventListener("click", function() {
+    document.getElementById("view").scrollIntoView({ behavior: "smooth" });
+});
+
+/*
+    intersection observer: when elements come into view
+    threshold (how much visibility)
+*/
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        // Optional: Stop observing after it's visible
+        observer.unobserve(entry.target);
+      }
+      else {
+    //  entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.15 // Trigger when 10% of element is visible
+  });
+
+  // Grab all elements you want to animate
+  document.querySelectorAll('.fade-in-on-scroll, .slide-in-on-scroll').forEach(el => {
+    observer.observe(el);
+  });
